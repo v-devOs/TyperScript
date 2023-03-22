c2881783d701713abf602c60fa73c865e01b3ede
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-03-22 15:10:18 -0600

    Tipos de datos typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-let message = "Uriel";
+class UsserAcount {
+    constructor(name, edad, id) {
+        this.name = name;
+        this.edad = edad;
+        this.id = id;
+    }
+}
+const myUser = new UsserAcount("Uriel", 19, 10);
+console.log(myUser.id);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 
-const saludar = ( nombre:string ): string => {
-  return `Hola ${nombre}`
+interface User{
+  name: string,
+  edad: number,
+  id: number
 }
+
+class UsserAcount{
+  name: string
+  edad: number
+  id: number
+
+  constructor( name: string, edad: number, id: number){
+    this.name = name
+    this.edad = edad
+    this.id = id
+  }
+}
+
+const myUser: User = new UsserAcount("Uriel", 19, 10);
+
+
+
+console.log(myUser.id);
